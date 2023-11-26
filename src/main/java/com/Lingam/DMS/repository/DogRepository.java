41f8bfe51c1b45cdcc1efe1559f218b3342d653b
JoyceLingam <joycemeyerlingam03@gmail.com>
2023-11-25 18:31:20 -0600

    added files

---
+++ src/main/java/com/Lingam/DMS/repository/DogRepository.java
@@ -0,0 +1,11 @@
+package com.Lingam.DMS.repository;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Lingam.DMS.Models.Dog;
+
+public interface DogRepository extends CrudRepository<Dog,Integer>{
+	List<Dog>findByName(String name);
+}
---
+++ src/main/java/com/Lingam/DMS/repository/TrainerRepository.java
@@ -0,0 +1,10 @@
+package com.Lingam.DMS.repository;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.Lingam.DMS.Models.Trainer;
+
+
+public interface TrainerRepository extends CrudRepository<Trainer, Integer> {
+
+}
